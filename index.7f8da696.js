77af444fcbacc8df4ff924ead7ff19d84ea3890c
ViktoriiaKhmelevska <116114782+ViktoriiaKhmelevska@users.noreply.github.com>
2023-04-16 08:54:57 +0000

    Deploying to gh-pages from @ SerhiiFrolikov/goit-js-hw-test@2e80004c9ecb5120ae23ee1313576d256f1c1c95 🚀

---
+++ index.7f8da696.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".todo"),list:document.querySelector(".todo__items")},o={};e.form.addEventListener("input",(function(e){const{name:t,value:n}=e.target;o[t]=n,console.log(o)}));
+//# sourceMappingURL=index.7f8da696.js.map
