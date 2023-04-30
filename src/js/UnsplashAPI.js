ceab2e1b8dc76a6c48466234285cf442bebb3e92
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-04-30 11:50:54 +0300

    search added

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -1,6 +1,7 @@
 export class UnsplashAPI{
     #BASE_URL = 'https://api.unsplash.com/search/photos';
     #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
+    #query = '';
     getPopularImages(page) {
         const url = `${this.#BASE_URL}?query=popular&page=${page}&per_page=12&orientation=portrait&client_id=${this.#API_KEY}`
         return fetch(url).then(response => {
@@ -9,5 +10,17 @@ export class UnsplashAPI{
         }
         return response.json();
         })
+    }
+    getImagesByQuery(page) {
+        const url = `${this.#BASE_URL}?query=${this.#query}&page=${page}&per_page=12&orientation=portrait&client_id=${this.#API_KEY}`
+        return fetch(url).then(response => {
+            if (!response.ok) {
+        throw new Error(response.status);
+        }
+        return response.json();
+        })
+    }
+    set query(newQuery) {
+        this.#query = newQuery;
     }
 }
